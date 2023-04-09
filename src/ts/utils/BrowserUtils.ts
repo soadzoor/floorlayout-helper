611f353b5a8d4abe1579ee53f6c62a515d08283c
Peter Varga <soadzoor@gmail.com>
2023-04-09 09:38:51 +0200

    Initial commit

--- src/ts/utils/BrowserUtils.ts
+++
@@ -1,7 +0,0 @@
-export class BrowserUtils
-{
-	public static get siteUrl()
-	{
-		return `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
-	}
-}
--- src/ts/utils/StringUtils.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-unused-modules
-export class StringUtils
-{
-	public static capitalize(value: string = "")
-	{
-		return value.charAt(0).toUpperCase() + value.slice(1);
-	}
-}
